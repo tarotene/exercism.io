281c5d06ee77a32aa27fbfa8cc4d765eb5672706
Kentaro Sugimoto <tarotene@gmail.com>
2021-10-03 00:54:07 +0900

    submit code typescript/rna-transcription

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,3 +1,22 @@
-export function toRna() {
-  throw new Error('Remove this statement and implement this function')
+export function toRna(dnaString: string): string {
+  let rnaString: string = "";
+  for (let dna of dnaString) {
+    switch (dna) {
+      case "G":
+        rnaString += "C";
+        break;
+      case "C":
+        rnaString += "G";
+        break;
+      case "T":
+        rnaString += "A";
+        break;
+      case "A":
+        rnaString += "U";
+        break;
+      default:
+        throw new Error("Invalid input DNA.");
+    }
+  }
+  return rnaString;
 }
